c4a044ce064389484220cf9cda2fce60a8c95f8a
Sudarshan Patil <p.sdrshn@gmail.com>
2025-04-05 21:38:17 +0530

    Enhance index.html with structured content and styling; add CSS and JavaScript for QR code functionality

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: "https://wa.me/+918956761543",
+        width: 256,
+        height: 256,
+        colorDark : "#2c3e50",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+});
